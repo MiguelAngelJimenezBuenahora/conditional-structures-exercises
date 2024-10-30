c0536e6127ac526542144532abd6afbfabaa0b35
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-30 17:39:49 -0500

    Solución del ejercicio "Palabra más larga"

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-#Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no.
+#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
 
-#Dividendo: 14
-#Divisor: 5
+#Palabra 1: edificio
+#Palabra 2: tren
+#La palabra edificio tiene 4 letras mas que tren.
 
-#La división no es exacta.
-#Cociente: 2
-#Resto: 4
+#Palabra 1: sol
+#Palabra 2: paralelepipedo
+#La palabra paralelepipedo tiene 11 letras mas que sol
 
-#Dividendo: 100
-#Divisor: 10
+#Palabra 1: plancha
+#Palabra 2: lapices
+#Las dos palabras tienen el mismo largo
 
-#La división es exacta.
-#Cociente: 10
-#Resto: 0
 
-dividend = int(input("""Welcome to that program, this program say you if the division is an exact division or no:
-                     please insert the dividend of division: """))
-divider = int(input("please insert thte divider of division: "))
-Restofdivision =dividend%divider    
-quotient = dividend//divider
-if Restofdivision <0:
-        print("the division isn't exact")
-elif Restofdivision >0:
-        print("the division isn't exact")
+# request two words to user
+word1 = input("Introduce la primera palabra: ")
+word2 = input("Introduce la segunda palabra: ")
+
+# Calcute the lenght of both words
+leng_word1 = len(word1)  #The sentence "len" is used for read the size of a date
+leng_word2 = len(word2)
+
+
+# Compare the lengths and determine which is longer
+if leng_word1 > leng_word2:
+    difference = leng_word1 - leng_word2
+    print(f"La palabra más larga es '{word1}' con {difference} letras más que '{word2}'.")
+elif leng_word2 > leng_word1:
+    difference = leng_word2 - leng_word1
+    print(f"La palabra más larga es '{word2}' con {difference} letras más que '{word1}'.")
 else:
-        print("the division is exact")
-print(f"""quotient: {quotient}
-rest: {Restofdivision}""")
-    
+    print("Ambas palabras tienen la misma longitud.")
 
