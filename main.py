86c801ccf979b81a9811f8c18350c9d1a3cf9d08
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-30 18:58:09 -0500

    Solución del ejercicio "Calculadora"

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-#Escriba un programa que determine si un caracter ingresado es letra, número, o ninguno de los dos. En caso que sea letra, determine si es mayúscula o minúscula.
 
-#Ingrese caracter: 9
-#Es numero.
 
-#Ingrese caracter: A
-#Es letra mayúscula.
+#Escriba un programa que simule una calculadora básica, este puede realizar operación de suma, resta, multiplicación y división.
 
-#Ingrese caracter: f
-#Es letra minúscula.
+#El programa debe recibir como entrada 2 números reales y un operador, que puede ser +, -, * o /.
 
-#Ingrese caracter: #
-#No es letra ni número.
+#La salida del programa debe ser el resultado de la operación.
 
-character =input("Enter a character: ")
-if character.isalpha(): #.isalpha is for check if the date is letter
-    if character.isupper(): #.isupper if for check if the date letter is on upper
-        typedate = "Upper"
+#Operando: 3
+#Operador: +
+#Operando: 2
+#3 + 2 = 5
+
+#Operando: 6
+#Operador: -
+#Operando: 7
+#6 - 7 = -1
+
+#Operando: 4
+#Operador: *
+#Operando: 5
+#4 * 5 = 20
+
+#Operando: 10
+#Operador: /
+#Operando: 4
+#10 / 4 = 2.5
+
+#Operando: -1
+#Operador: **
+#Operando: 4
+#-1 ** 4 = 1
+
+number1 = float(input("""In this program you can input two number and insert a operator and solve the operation 
+Please insert a number: """))
+operator = input("Please insert the operator: ")
+number2 = float(input("Please insert the other number: "))
+if operator == '-':
+    result = number1-number2
+elif operator == '+':
+    result = number1+number2
+elif operator == 'x' or '*':
+    result = number1*number2
+elif operator == '/':
+    if number2 !=0: #Verivy if doesn't 0 the number2
+        result = number1/number2
     else:
-        typedate = "minuscule"
-    print(f"""The character '{character}' is a letter and is {typedate}""")
-elif character.isdigit():   #Verify if the dare is type number
-    print(f"""The character '{character}' is a number""")
-else: #any of two
-    print(f"""The character '{character}' isn't letter or number.""")
+        result ="Error, division on 0"
+else:
+    result = "Operator invalid"
+print(f"""The result for operation between {number1} {operator} {number2} is: {result}""")
+
