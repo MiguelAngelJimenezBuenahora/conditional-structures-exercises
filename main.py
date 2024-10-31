b52a9fe425fabfe0defa7448b842877fdca195c8
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-30 19:49:54 -0500

    Solución del ejercicio "Edad"

--- main.py
+++ main.py
@@ -1,52 +1,50 @@
+#Escriba un programa que entregue la edad del usuario a partir de su fecha de nacimiento:
+
+#Ingrese su fecha de nacimiento.
+#Dia: 14
+#Mes: 6
+#Anno: 1948
+#Usted tiene 62 annos
+
+#Por supuesto, el resultado entregado depende del día en que su programa será ejecutado.
+
+#Para obtener la fecha actual, puede hacerlo usando la función localtime que viene en el módulo time. Los valores se obtienen de la siguiente manera (suponiendo que hoy es 11 de marzo de 2011):
+
+#>>> from time import localtime
+#>>> t = localtime()
+#>>> t.tm_mday
+#11
+#>>> t.tm_mon
+#3
+#>>> t.tm_year
+#2011
+
+#El programa debe tener en cuenta si el cumpleaños ingresado ya pasó durante este año, o si todavía no ocurre.
+
+import time
+#In case do you want use this code with the date of system, 
+#you can use:
+date_current = time.localtime()
+anio_exercise = date_current.tm_year
+month_exercise = date_current.tm_mon
+day_exercise = date_current.tm_mday
 
 
-#Escriba un programa que simule una calculadora básica, este puede realizar operación de suma, resta, multiplicación y división.
+#anio_exercise = 2011
+#month_exercise = 3
+#day_exercise = 11
 
-#El programa debe recibir como entrada 2 números reales y un operador, que puede ser +, -, * o /.
 
-#La salida del programa debe ser el resultado de la operación.
+dateofborn = input("""This program process your age of born and say you what is your age, 
+Please can you insert you borntime in this format (dd/mm/aaaa): """)
 
-#Operando: 3
-#Operador: +
-#Operando: 2
-#3 + 2 = 5
+#I'll Convert string to a date object
 
-#Operando: 6
-#Operador: -
-#Operando: 7
-#6 - 7 = -1
+day, month, anio = map(int, dateofborn.split('/')) 
 
-#Operando: 4
-#Operador: *
-#Operando: 5
-#4 * 5 = 20
+yearfromborntocurrent = anio_exercise - anio
 
-#Operando: 10
-#Operador: /
-#Operando: 4
-#10 / 4 = 2.5
-
-#Operando: -1
-#Operador: **
-#Operando: 4
-#-1 ** 4 = 1
-
-number1 = float(input("""In this program you can input two number and insert a operator and solve the operation 
-Please insert a number: """))
-operator = input("Please insert the operator: ")
-number2 = float(input("Please insert the other number: "))
-if operator == '-':
-    result = number1-number2
-elif operator == '+':
-    result = number1+number2
-elif operator == 'x' or '*':
-    result = number1*number2
-elif operator == '/':
-    if number2 !=0: #Verivy if doesn't 0 the number2
-        result = number1/number2
-    else:
-        result ="Error, division on 0"
-else:
-    result = "Operator invalid"
-print(f"""The result for operation between {number1} {operator} {number2} is: {result}""")
-
+if anio_exercise == anio:
+    if (month_exercise, day_exercise) < (month, day):
+        yearfromborntocurrent -= 1
+print(f"""Your have {yearfromborntocurrent} annos""") 
